dbf555ab6420187d87c6c9c4b8470d51bd3b8aaf
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-03-26 15:24:57 +0100

    add layered traits example

---
+++ src/main/scala/com/traits/LayeredTraits.scala
@@ -0,0 +1,75 @@
+package com.traits
+
+trait Logger {
+  def log(message: String) { }
+}
+
+trait ConsoleLogger extends Logger {
+  override def log(message: String) = println(message)
+}
+
+trait TimestampLogger extends Logger {
+  override def log(message: String): Unit = {
+    super.log(new java.util.Date() + " " + message)
+  }
+}
+
+trait ShortLogger extends Logger {
+  val maxLength = 15
+
+  override def log(message: String): Unit = {
+    super.log(
+      if (message.length <= maxLength)
+        message
+      else
+        message.substring(0, maxLength - 3) + "..."
+    )
+  }
+}
+
+class SavingAccount extends Logger {
+  var balance = 0.0
+
+  def credit(amount: Double) = {
+    balance += amount
+    balance
+  }
+
+  def withdraw(amount: Double) = {
+    if (amount > balance)
+      log("Insufficient funds")
+    else {
+      log("Withdrew " + amount + "$.")
+      balance -= amount
+      balance
+    }
+  }
+}
+
+object SavingAccountWithLogger extends App {
+  val account = new SavingAccount
+  println(account.withdraw(3.3))
+  println(account.credit(4.4))
+  println(account.withdraw(3.3))
+}
+
+object SavingAccountWithConsoleLogger extends App {
+  val account = new SavingAccount with ConsoleLogger
+  println(account.withdraw(3.3))
+  println(account.credit(4.4))
+  println(account.withdraw(3.3))
+}
+
+object SavingAccountWithConsoleLoggerWithTimestampLoggerWithShortLogger extends App {
+  val account = new SavingAccount with ConsoleLogger with TimestampLogger with ShortLogger
+  println(account.withdraw(3.3))
+  println(account.credit(4.4))
+  println(account.withdraw(3.3))
+}
+
+object SavingAccountWithConsoleLoggerWithShortLoggerWithTimestampLogger extends App {
+  val account = new SavingAccount with ConsoleLogger with ShortLogger with TimestampLogger
+  println(account.withdraw(3.3))
+  println(account.credit(4.4))
+  println(account.withdraw(3.3))
+}
